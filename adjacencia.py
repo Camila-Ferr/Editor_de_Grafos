1af906618877693fd7ffc6b17f823d53d950b20a
CaubiLoureiro <caubiloureiro@id.uff.br>
2021-12-02 19:33:58 -0300

    Update adjacencia.py

--- adjacencia.py
+++ adjacencia.py
@@ -8,6 +8,7 @@ class Adjacente:
         self.escreve_adjacente()
 
     def escreve_adjacente(self):
+        self.matriz_adjacente = []
         for i in range(self.maior_vertice):
             lista = [0] * self.maior_vertice
             self.matriz_adjacente.append(lista)
@@ -21,6 +22,7 @@ class Adjacente:
 
         novos_vertices = [vert1, vert2, peso_arest]
         self.entradas.append(novos_vertices)
+        self.atualiza_maior()
         self.escreve_adjacente()
 
 
@@ -29,6 +31,7 @@ class Adjacente:
         for i in range(len(self.entradas)):
             if(self.entradas[i][0]==vert1 and self.entradas[i][1]==vert2):
                 self.entradas.pop(i)
+                self.atualiza_maior()
                 self.escreve_adjacente()
 
 
@@ -41,6 +44,12 @@ class Adjacente:
                 self.escreve_adjacente()
 
 
+    def atualiza_maior(self):
 
+        for i in range(len(self.entradas)):
+            if(self.entradas[i][0] > self.maior_vertice):
+                self.maior_vertice = self.entradas[i][0]
 
+            elif(self.entradas[i][1] > self.maior_vertice):
+                self.maior_vertice = self.entradas[i][1]
 
