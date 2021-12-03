70d1e76931ab4ac961952d0c75be83e32d04193e
CaubiLoureiro <caubiloureiro@id.uff.br>
2021-12-02 23:22:47 -0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -4,26 +4,41 @@ import PySimpleGUI as sg
 class Tela:
 
     def __init__(self):
+        sg.theme(new_theme='Reddit')
+        sg.theme_border_width(border_width = 5)
 
+        sg.theme_background_color(color = 'Pink')
 
         #Layout
         layout = [
-            [sg.Text('Vértice 1:', size=(10,0)), sg.Input(size=(15,1)), sg.Text('Peso do Vértice 1:', size=(10,0)), sg.Input(size=(15,1))],
-            [sg.Text('Vértice 2:', size=(10,0)), sg.Input(size=(15,1)), sg.Text('Peso do Vértice 2',size=(10,0)), sg.Input(size=(15,1))],
-            [sg.Text('Comprimento da Aresta:', size=(10,0)), sg.Input(size=(15,1))],
-            [sg.Button('Adicionar'), sg.Button('Remover Aresta'), sg.Button('Modificar Aresta') ]
+            [sg.Text('Adicionar', size=(10,0), background_color='Pink', justification='left'), sg.Text(" "*90, background_color='Pink'), sg.Text('Remover Aresta')],
+            [sg.Text('Vértice 1:', size=(10,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='vertice1'), sg.Text('Peso do Vértice 1:', size=(10,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='peso1'),
+             sg.Text(" "*5, background_color='Pink'), sg.Text('Vértice 1:', size=(0,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='vertice1_mod')],
+
+            [sg.Text('Vértice 2:', size=(10,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='vertice1'), sg.Text('Peso do Vértice 2:',size=(10,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='peso2')],
+            [sg.Text('Comprimento da Aresta:', size=(10,0), background_color='Pink', justification='left'), sg.Input(size=(10,1), key='comprimento')],
+            [sg.Button('Adicionar', key='adicionar'), sg.Button('Remover Aresta',  key='remove_aresta'), sg.Button('Modificar Aresta', key='modifica_aresta') ]
 
         ]
 
+
         #janela
 
-        window = sg.Window("Editor de Grafos", size=(1500,800)).layout(layout)
+        self.window = sg.Window("Editor de Grafos", size=(1500, 950)).layout(layout)
 
-        #Extrair os dados da tela
-        self.button, self.values = window.Read()
+
 
     def iniciar(self):
-        print(self.values)
+        while True:
+            # Extrair os dados da tela
+            self.button, self.values = self.window.Read()
+
+            if self.button == sg.WIN_CLOSED:
+                break
+
+            #if ##PROGRAMAR OS EVENTOS########
+
+
 
 
 
